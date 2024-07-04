efc2fdb45e1eae78f214ea4bd7bea966a2d94a50
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-07-04 16:13:20 +0100

    add setters joueur class

--- Classes/Joueur.php
+++ Classes/Joueur.php
@@ -43,6 +43,39 @@ class Joueur
     {
         return $this->equipe;
     }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @param mixed $dateNaissance
+     */
+    public function setDateNaissance($dateNaissance)
+    {
+        $this->dateNaissance = $dateNaissance;
+    }
+
+    /**
+     * @param mixed $position
+     */
+    public function setPosition($position)
+    {
+        $this->position = $position;
+    }
+
+    /**
+     * @param mixed $equipe
+     */
+    public function setEquipe($equipe)
+    {
+        $this->equipe = $equipe;
+    }
+    
     public function getDetails()
     {
         echo "Joueur: $this->nom, Date de naissance: $this->dateNaissance, Position: $this->position, Equipe: $this->equipe";
