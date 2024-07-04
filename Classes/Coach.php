364c5caae0957eb8c1396d7e27e440b58181cae7
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-07-04 16:17:32 +0100

    added getters and setters coach class

--- Classes/Coach.php
+++ Classes/Coach.php
@@ -7,6 +7,56 @@ class Coach extends Person
         parent::__construct($nom, $adresse, $tel);
     }
 
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @param mixed $adresse
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @param mixed $tel
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+    }
+
+
     public function getDetails()
     {
         echo "Le Coach: $this->nom, son adresse est: $this->adresse, son tel: $this->tel";
