a47377ef181f5d7a6bf9e5cc644532e4a6881d41
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-07-04 16:18:39 +0100

    getter & setter arbitre class

--- Classes/Arbitre.php
+++ Classes/Arbitre.php
@@ -7,6 +7,54 @@ class Arbitre extends Person
         parent::__construct($nom, $adresse, $tel);
     }
 
+    /**
+     * @return mixed
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTel()
+    {
+        return $this->tel;
+    }
+
+    /**
+     * @param mixed $nom
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+    }
+
+    /**
+     * @param mixed $adresse
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+    }
+
+    /**
+     * @param mixed $tel
+     */
+    public function setTel($tel)
+    {
+        $this->tel = $tel;
+    }
+
     public function getDetails()
     {
         echo "L'Arbitre: $this->nom, Adresse est: $this->adresse, Tel: $this->tel";
