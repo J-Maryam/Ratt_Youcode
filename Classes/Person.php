ca6d475c251c9b81e5d9624b8877855d74866dd3
J-Maryam <132862036+J-Maryam@users.noreply.github.com>
2024-07-04 13:33:44 +0100

    class Person

---
+++ Classes/Person.php
@@ -0,0 +1,16 @@
+<?php
+
+abstract class Person
+{
+    protected $nom, $adresse, $tel;
+
+    public function __construct($nom, $adresse, $tel)
+    {
+        $this->nom = $nom;
+        $this->adresse = $adresse;
+        $this->tel = $tel;
+    }
+
+    abstract public function getDetails();
+
+}
